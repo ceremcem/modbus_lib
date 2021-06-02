883af82446255803d80acef2f567b712879e936a
Cerem Cem ASLAN <cem@aktos.io>
2021-06-02 08:44:14 +0300

    added crc_ok() checking

--- modbus_lib.c
+++ modbus_lib.c
@@ -13,8 +13,10 @@
 
 uint8_t g_modbus_lib_received_telegram[MODBUS_LIB_MAX_BUFFER]; 
 uint16_t g_modbus_lib_received_length = 0; 
+ModbusConfig_t* config; 
 
 int modbus_lib_init(ModbusConfig_t* cfg){
+    config = cfg; 
     return 0; 
 }
 
@@ -23,7 +25,22 @@ void modbus_lib_append_data(uint8_t byte){
 }
 
 void modbus_lib_end_of_telegram(){
-    volatile uint16_t x = usMBCRC16(g_modbus_lib_received_telegram, g_modbus_lib_received_length-2);
+    if (crc_ok()){
+        0; // debugger: printf "crc okay\n"
+    } else {
+        0; // debugger: printf "wrong crc!\n"
+    }
     g_modbus_lib_received_length = 0; // debugger: p/x *g_modbus_lib_received_telegram@g_modbus_lib_received_length
-    (void) 0; // debugger 
+}
+
+inline int crc_ok(){
+    uint16_t expected = usMBCRC16(g_modbus_lib_received_telegram, g_modbus_lib_received_length-2);
+    uint16_t got = (g_modbus_lib_received_telegram[g_modbus_lib_received_length-1] << 8) \
+        | g_modbus_lib_received_telegram[g_modbus_lib_received_length-1];
+
+    if (got == expected){
+        return 1; 
+    } else {
+        return 0;
+    }
 }
